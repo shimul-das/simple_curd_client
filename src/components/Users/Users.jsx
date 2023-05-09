c2ab50405d05d7dbcb26267c6bb38007d764eefe
Shimul Das <shimul.191002110@gmail.com>
2023-05-09 23:48:01 +0600

    we have mongobd delete option from database

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,11 +3,20 @@ import { useLoaderData } from 'react-router-dom'
 
 const Users = () => {
     const users=useLoaderData()
+    const handleDelete=(_id)=>{
+        console.log("delete",_id)
+        fetch(`http://localhost:5000/user/${_id}`,{
+            method:"DELETE"
+        })
+        .then(res=>res.json())
+        .then(data=>console.log(data))
+
+    }
   return (
     <div>
         <h2>{users.length}</h2>
         {
-            users.map(u=><p key={u._id}>{u.name}:{u.email}</p>)
+            users.map(u=><p key={u._id}>{u.name}:{u.email}: {u._id} <button onClick={()=>handleDelete(u._id)}>X</button></p>)
         }
     </div>
   )
